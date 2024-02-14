fa33a829d523644eac5f72bdbaf13b2f435fcfc3
Stéphane Caron <stephane.caron@normalesup.org>
2024-02-14 18:28:25 +0100

    Update example environment to latest version

--- agent/agent.py
+++ agent/agent.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#
-# Copyright 2023 Inria
-
-"""Tiny example: balancing using a proportional wheel controller."""
 
 import gymnasium as gym
 import numpy as np
-
 import upkie.envs
 
 upkie.envs.register()
 
 if __name__ == "__main__":
-    with gym.make("UpkieGroundVelocity-v1", frequency=200.0) as env:
+    with gym.make("UpkieGroundVelocity-v3", frequency=200.0) as env:
         env.reset()  # connects to the spine
         action = np.zeros(env.action_space.shape)
         for step in range(1_000_000):
