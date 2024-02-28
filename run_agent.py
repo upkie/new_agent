98e63577a3a1e21681c01a70e66a245b4b36291f
Stéphane Caron <stephane.caron@inria.fr>
2024-02-28 23:28:29 +0100

    Configure agent process for the Raspberry Pi

--- run_agent.py
+++ run_agent.py
@@ -5,9 +5,15 @@ import gymnasium as gym
 import numpy as np
 import upkie.envs
 
+from upkie.utils.raspi import configure_agent_process, on_raspi
+
 upkie.envs.register()
 
+
 if __name__ == "__main__":
+    if on_raspi():
+        configure_agent_process()
+
     with gym.make("UpkieGroundVelocity-v3", frequency=200.0) as env:
         env.reset()  # connects to the spine
         action = np.zeros(env.action_space.shape)
