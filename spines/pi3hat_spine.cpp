5431c5abe2bb05ff51752b19e8ab09cfa5b366ca
Stéphane Caron <stephane.caron@inria.fr>
2024-03-18 17:29:17 +0100

    Update example pi3hat spine

--- spines/pi3hat_spine.cpp
+++ spines/pi3hat_spine.cpp
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2022 Stéphane Caron
-// Copyright 2023 Inria
+// Copyright 2023-2024 Inria
 
+#include <unistd.h>
 #include <vulp/actuation/Pi3HatInterface.h>
 #include <vulp/observation/ObserverPipeline.h>
 #include <vulp/observation/sources/CpuTemperature.h>
@@ -144,6 +145,19 @@ inline bool calibration_needed() {
   return !file_found;
 }
 
+inline const std::string get_log_path(const std::string& log_dir) {
+  const auto now = upkie::utils::datetime_now_string();
+  const std::string prefix = log_dir + "/" + now + "_pi3hat_spine";
+
+  char hostname[512];
+  int return_code = gethostname(hostname, 512);
+  if (return_code != 0) {
+    spdlog::error("Could not get the robot's hostname, errno = {}", errno);
+    return prefix + ".mpack";
+  }
+  return prefix + "_" + hostname + ".mpack";
+}
+
 int main(const CommandLineArguments& args) {
   if (calibration_needed()) {
     spdlog::error("Calibration needed: did you run `upkie_tool rezero`?");
@@ -205,8 +219,7 @@ int main(const CommandLineArguments& args) {
     Spine::Parameters spine_params;
     spine_params.cpu = args.spine_cpu;
     spine_params.frequency = args.spine_frequency;
-    const auto now = upkie::utils::datetime_now_string();
-    spine_params.log_path = args.log_dir + "/" + now + "_pi3hat_spine.mpack";
+    spine_params.log_path = get_log_path(args.log_dir);
     spdlog::info("Spine data logged to {}", spine_params.log_path);
     Spine spine(spine_params, interface, observation);
     spine.run();
